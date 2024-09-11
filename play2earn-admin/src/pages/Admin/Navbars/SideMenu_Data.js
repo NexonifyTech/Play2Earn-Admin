9b3904eb7bb49a428f82eb88f8e3318845db4c86
balasakthi <balaabimanyugnc@gmail.comgit config --global user.name>
2024-09-11 21:33:00 +0530

    project created

---
+++ play2earn-admin/src/pages/Admin/Navbars/SideMenu_Data.js
@@ -0,0 +1,15 @@
+
+import { MdOutlineAdminPanelSettings } from "react-icons/md";
+
+
+
+export const sidebarItems = [
+  {
+    "id": 1,
+    "label": " Dashboard",
+    "parent_id": null,
+    "icon":<MdOutlineAdminPanelSettings size={20} />,
+    "order_index": 1,
+    "url": "/admin/dashboard"
+  },
+];
